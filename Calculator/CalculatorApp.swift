35d0eb2cb966d480490f6772878e3a2b25a46803
ondrejtoman <jsem@ondrejtoman.cz>
2022-10-29 18:39:25 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ondřej Toman on 29.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
